401f49c99995d5e64f1271b4907bcdaad0b2a419
Cole-OBalle <44816333+Cole-OBalle@users.noreply.github.com>
2018-11-10 16:51:19 -0800

    Add files via upload

---
+++ RPG Real/src/RPG/Character.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RPG;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Polygon;
+import java.awt.Toolkit;
+import java.io.File;
+
+public class Character {
+    protected int xPos;
+    protected int yPos;
+    protected Image image;
+    
+    Character()
+    {
+    }
+}
---
+++ RPG Real/src/RPG/Player.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RPG;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Polygon;
+import java.awt.Toolkit;
+import java.io.File;
+public class Player extends Character {
+    Player(Image _image){
+        image = _image;
+    }
+    public void Draw(){
+        
+    }
+}
